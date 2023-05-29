164e4fc9407b4b8e2b2282c538319be3db3e45c7
Fethers1805 <78319723+fethers1805@users.noreply.github.com>
2023-05-28 20:46:00 -0400

    Clean up old variable and comment

--- script/script.js
+++ script/script.js
@@ -1,23 +1,4 @@
 const gameChoices = ['rock', 'paper', 'scissors'];
-let playerChoiceValid = 0;
-/* 
-
-INITIAL FUNCTION FOR COMPUTER CHOICE
-
-function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3);
-    if (randomNumber === 0) {
-        return "rock";
-    }
-    else if (randomNumber === 1) {
-        return "paper";
-    }
-    else {
-        return "scissors";
-    }
-}
-
-*/
 
 function getComputerChoice() {
     // Pick a random spot from the array of options a participant can choose from.
