ce871bffb93431a13dafffb75abeb023126fa009
Fethers1805 <78319723+fethers1805@users.noreply.github.com>
2023-05-28 20:19:57 -0400

    Add player choice with verification

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
-const gameChoices = ["rock", "paper", "scissors"];
-
+const gameChoices = ['rock', 'paper', 'scissors'];
+let playerChoiceValid = 0;
 /* 
 
 INITIAL FUNCTION FOR COMPUTER CHOICE
@@ -23,5 +23,35 @@ function getComputerChoice() {
     // Pick a random spot from the array of options a participant can choose from.
     return gameChoices[Math.floor(Math.random()*3)];
 }
-console.log(getComputerChoice());
+
+function getPlayerChoice() {
+    // Get and clean the player's choice
+    return prompt("What would you like to pick; rock, paper or scissors? ", "").toString().toLowerCase();
+
+}
+
+function verifyPlayerChoice(choiceToVerify) {
+    if (gameChoices.includes(choiceToVerify)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function compareChoices() {
+    let compChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
+    verifyPlayerChoice(playerChoice);
+    while (verifyPlayerChoice(playerChoice) != true) {
+        console.log(`${playerChoice} is an invalid selection, please make a valid selection`);
+        playerChoice = getPlayerChoice(); 
+    }
+    return playerChoice;
+
+}
+console.log(compareChoices());
+// console.log(getPlayerChoice());
+// console.log(getPlayerChoice());
+// console.log(getComputerChoice());
 
